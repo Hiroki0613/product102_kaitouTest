969922ce35256551a7a62196034a3a9108cf2fa4
Hiroki Kondo <kondo.matr02@gmail.com>
2019-10-08 19:31:56 +0900

    Initial Commit

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'product102_kaitou' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for product102_kaitou
+
+
+pod 'GLScratchCard'
+
+end
---
+++ product102_kaitou/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  product102_kaitou
+//
+//  Created by 宏輝 on 08/10/2019.
+//  Copyright © 2019 Hiroki Kondo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
